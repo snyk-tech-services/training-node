37c6cd57dcdbae13983ea1c86abf0c6405811f42
aarlaud <antoine@snyk.io>
2020-02-03 16:30:52 -0500

    feat: test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send("Hello world"))
-
-app.listen(port, () => console.log("Example app listening on port ${port}!"))
+app.get('/antoine', (req,res) => res.send("Antoine here again"))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
