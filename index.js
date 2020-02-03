2c76eb9b66d8902b20a8a11f334e8476dd7ff686
Matt @ Snyk <59509697+snyk-matt@users.noreply.github.com>
2020-02-03 17:10:54 -0500

    edited index.js to Matt

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const port = 3000
 
 app.get('/', (req, res) => res.send("Hello world"))
 
-app.get('/antoine', (req,res) => res.send("Antoine here again"))
+app.get('/Matt', (req,res) => res.send("Matt here again"))
 app.listen(port, () => console.log("Example app listening on port %s!",port))
