6ab34ae442cdc99079655a4b25c7a556e9a2a8aa
Scott Strool <Scott.Strool@snyk.io>
2020-02-03 16:24:36 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 console.log("Hello world");
+=======
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send("Hello world"))
+app.get('/scott', (req, res) => res.send("Who wants sushi for dinner?"))
+
+app.listen(port, () => console.log("Example app listening on port ${port}!"))
+>>>>>>> Stashed changes
