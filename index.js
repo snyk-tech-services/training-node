04871f6de2eda858c27e50617420b96fe0f71025
roberthicks <roberthicksiii@gmail.com>
2020-02-03 16:21:38 -0500

    added endpoint rob

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send("Hello world"))
-
+app.get('/rob', (req, res) => res.send("This better work"))
 app.listen(port, () => console.log("Example app listening on port ${port}!"))
