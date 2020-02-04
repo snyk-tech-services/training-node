3b68ecbb13208c4e207b24a71bebb132d957ae80
Clinton Herget <clinton.herget@snyk.io>
2020-02-04 10:49:43 -0500

    feat: store bestified names in redis and print a list

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express')
 const functions = require('./functions')
 const app = express()
 const port = 3000
+const redis_port = 6379
+const redis_host = process.env.REDIS_HOST
+
+const redis = require('redis')
+
+redis_client = redis.createClient({
+  port: redis_port,
+  host: redis_host,
+});
 
 app.set('view engine', 'pug')
 
 app.get('/', function(req, res) {
 	nameToBestify = req.query.name ? req.query.name : 'Someone'
-	res.render('page', { title: "Home", headline: functions.bestify(nameToBestify) })
+
+    redis_client.get('names', function(err, names) {
+
+      names_list = names ? JSON.parse(names) : []
+
+      names_list.push(nameToBestify)
+      redis_client.set('names', JSON.stringify(names_list));
+
+      res.render('page', { title: "Home", headline: functions.bestify(nameToBestify), names: names_list })
+
+    });
 })
 
 app.get('/antoine', (req,res) => res.render('page', { title: "Antoine's Page", headline: functions.bestify('Antoine') }))
